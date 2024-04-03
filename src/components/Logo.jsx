cc522e825de199115ec00f975d1429d9217a2b98
Aakash <aakashanand1995@gmail.com>
2024-04-03 12:04:08 +0530

    fixed logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = ({ width = "50px", height = "20px" }) => {
       <img
         height={height}
         width={width}
-        src="/src/assets/logo.png"
+        src="https://cloud.appwrite.io/v1/storage/buckets/66050a4a33fcd5463798/files/660cf80ab1c0468183d4/view?project=660508f8ae80f6730c08&mode=admin"
         alt="logo of the website"
       />
     </div>
